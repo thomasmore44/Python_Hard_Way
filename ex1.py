36e9ccdfb10e442fdb0f4e98173b36f531058f81
Thomas Cox <thomascox@nicoles-air.faculty.heights.edu>
2017-01-31 13:38:09 -0500

    My first commit

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# print "Hello World!"
+# print "Hello again"
+# print "I like typing this."
+# print "This is fun."
+# print "This is fun."
+# print "Yay! Printing.'not'"
+# print 'I "said" do not touch this.'
+print "This is an additional test line which Zed asked me to write"
+
+# This is a comment...
---
+++ ex2.py
@@ -0,0 +1,6 @@
+print "I could have code like this" # and a comment after is ignored
+
+# This is a comment to disable a line
+# print "This won't run"
+
+print "This will run"
